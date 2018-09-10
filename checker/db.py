94cf95194ad8c486958d46278803d6f8e4cd0aa6
IvanovaJA <ivanovaja@rbtechnologies.ru>
2018-09-10 17:03:49 +0300

    Fix docstring in db.py

--- checker/db.py
+++ checker/db.py
@@ -3,11 +3,9 @@ from flask_pymongo import PyMongo
 
 
 def get_db():
-    """Connect to the application's configured database. The connection
-    is unique for each request and will be reused if this is called
-    again.
+    """
+    Get the application's configured database.
     """
     if 'db' not in g:
         g.db = PyMongo(current_app)
     return g.db
-
