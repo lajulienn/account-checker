a34453c51f25944aea35da099c51e5586bf38fbf
IvanovaJA <ivanovaja@rbtechnologies.ru>
2018-09-10 17:48:53 +0300

    Remove separate create_app method from init

--- checker/__init__.py
+++ checker/__init__.py
@@ -4,22 +4,19 @@ from flask import Flask
 from checker import checker
 
 
-def create_app():
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        MONGO_DBNAME='checker_db',
-        MONGO_URI='mongodb://localhost:27017/checker_db',
-    )
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    MONGO_DBNAME='checker_db',
+    MONGO_URI='mongodb://localhost:27017/checker_db',
+)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    # apply the blueprints to the app
-    app.register_blueprint(checker.bp)
-
-    return app
+# apply the blueprints to the app
+app.register_blueprint(checker.bp)
